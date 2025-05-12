bba902f2dc81e03871701925372d79e8e51a29d2
Federico Stra <stra.federico@gmail.com>
2025-05-12 21:22:30 +0200

    test: constructors and returned type

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,41 @@
 using ProtectedArrays
-using Test
+using Test, TestSetExtensions, SafeTestsets
 
-@testset "ProtectedArrays.jl" begin
-    # Write your tests here.
+@testset ExtendedTestSet "ProtectedArrays.jl" begin
+    @safetestset "Constructors" begin
+        using ProtectedArrays
+
+        a = [1, 2]
+        pa = protect(a)
+        @test unprotect(pa) === a
+        @test parent(pa) === a
+        @test isa(pa, ProtectedArray{Int,1,Vector{Int}})
+        @test isa(pa, ProtectedArray{Int,1})
+        @test isa(pa, ProtectedArray{Int})
+        @test isa(pa, ProtectedArray)
+        @test isa(pa, ProtectedVector{Int,Vector{Int}})
+        @test isa(pa, ProtectedVector{Int})
+        @test isa(pa, ProtectedVector)
+
+        a = [1 2 3; 4 5 6]
+        pa = protect(a)
+        @test unprotect(pa) === a
+        @test parent(pa) === a
+        @test isa(pa, ProtectedArray{Int,2,Matrix{Int}})
+        @test isa(pa, ProtectedArray{Int,2})
+        @test isa(pa, ProtectedArray{Int})
+        @test isa(pa, ProtectedArray)
+        @test isa(pa, ProtectedMatrix{Int,Matrix{Int}})
+        @test isa(pa, ProtectedMatrix{Int})
+        @test isa(pa, ProtectedMatrix)
+
+        a = reshape(collect(1:24), (2,3,4))
+        pa = protect(a)
+        @test unprotect(pa) === a
+        @test parent(pa) === a
+        @test isa(pa, ProtectedArray{Int,3,Array{Int,3}})
+        @test isa(pa, ProtectedArray{Int,3})
+        @test isa(pa, ProtectedArray{Int})
+        @test isa(pa, ProtectedArray)
+    end
 end
