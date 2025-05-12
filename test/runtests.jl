7880b3e78a7b5ccd58caee8412fc2677d711b31c
Federico Stra <stra.federico@gmail.com>
2025-05-12 21:47:27 +0200

    test: basic indexing

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,4 +38,52 @@ using Test, TestSetExtensions, SafeTestsets
         @test isa(pa, ProtectedArray{Int})
         @test isa(pa, ProtectedArray)
     end
+
+    @testset "Interfaces" begin
+        @safetestset "Iteration" begin end
+        @safetestset "Indexing" begin
+            using ProtectedArrays
+
+            err_msg = "`ProtectedArray` does not allow modifying elements with `setindex!`"
+
+            a = [1, 2, 3, 4, 5, 6]
+            a_backup = copy(a)
+            pa = protect(a)
+            for i in 1:6
+                @test pa[i] == i
+            end
+            for i in 1:6
+                @test_throws err_msg pa[i] = 0
+            end
+            @test_throws err_msg pa .= 0
+            @test_throws err_msg reverse!(pa)
+            @test_throws MethodError push!(pa, 0)
+            @test_throws MethodError pushfirst!(pa, 0)
+            @test_throws MethodError pop!(pa)
+            @test_throws MethodError popat!(pa, 3)
+            @test_throws MethodError popfirst!(pa)
+            @test_throws MethodError insert!(pa, 3, 0)
+            @test_throws MethodError splice!(pa, 3, 0)
+            @test_throws MethodError deleteat!(pa, 3)
+            @test_throws MethodError resize!(pa, 3)
+            @test parent(pa) === a
+            @test parent(pa) == a_backup
+
+            a = [1 3 5; 2 4 6]
+            a_backup = copy(a)
+            pa = protect(a)
+            for i in 1:6
+                @test pa[i] == i
+            end
+            for i in 1:6
+                @test_throws err_msg pa[i] = 0
+            end
+            @test_throws err_msg pa .= 0
+            @test_throws err_msg reverse!(pa)
+            @test parent(pa) === a
+            @test parent(pa) == a_backup
+        end
+        @safetestset "Abstract array" begin end
+        @safetestset "Strided array" begin end
+    end
 end
